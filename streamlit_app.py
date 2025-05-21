8a59e8ee8cea085d2a57da0dfeb651462710f1d1
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-05-21 14:31:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,10 @@ st.title("Aplikasi Sederhana")
 #col1, col2 = st.columns(2)
 
 #with col1:
-    st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
-    angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
-    if (angka % 2) == 0:
-        st.write(f"{angka} adalah Bilangan Genap")
-    else:
-        st.write(f"{angka} adalah Bilangan Ganjil")
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah Bilangan Genap")
+else:
+  st.write(f"{angka} adalah Bilangan Ganjil")
