fcdce7cf277832f7c17a1b0f6ad5d0dcecb02880
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-05-21 09:32:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Selamat Datang di Web Informatika !")
 st.write(
     "ngodingseru bersama Bapak Hendri Setiadi"
 )
-st.image("transparent_2024-08-03T14-11-31.png", width=200)
+st.image("miku.jpg", width=200)
