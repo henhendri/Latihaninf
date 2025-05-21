85ec86a6805a4b0179fb939e0ad9a84666a4b7af
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-05-21 14:29:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,17 @@ st.write(
     "ngodingseru bersama Bapak Hendri Setiadi"
 )
 st.image("miku.jpg", width=200)
+
+st.title("Aplikasi Sederhana")
+
+# Menggunakan layout kolom
+#col1, col2 = st.columns(2)
+
+#with col1:
+    st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+    angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+    if (angka % 2) == 0:
+        st.write(f"{angka} adalah Bilangan Genap")
+    else:
+        st.write(f"{angka} adalah Bilangan Ganjil")
